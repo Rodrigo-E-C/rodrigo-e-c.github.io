f97864fe97a24a936a5ac54497022ec833d34913
Rodrigo-E-C <rodriestradacastillo197@gmail.com>
2024-11-17 15:18:56 -0600

    Red Sotecc
    DUAL

---
+++ Productos.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const closeBtn = document.querySelector('.close-btn');
+    const menu = document.querySelector('nav');
+
+    menuBtn.addEventListener('click', function () {
+        menu.classList.toggle('menu-open');
+        menuBtn.style.display = 'none';
+        closeBtn.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function () {
+        menu.classList.toggle('menu-open');
+        menuBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+    });
+});
